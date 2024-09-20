37a85788d2929fa9362f693a544ac3cb56d36c09
Mark Buckle <markbuckle92@gmail.com>
2024-09-20 08:52:25 -0230

    Upload transferred files

---
+++ IntroProject/Models/BaseModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Models
+{
+	public class BaseModel
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+
+		public BaseModel()
+		{
+			Name = string.Empty; // Initialize with a default value
+		}
+	}
+}
